f0b8666e90bd84b7a5d179f42e01cb9b5e0c38e7
Hajdu Péter <hajdupetke@gmail.com>
2023-03-14 12:48:35 +0100

    modified image div background color for lighter overlay

--- src/components/ClothingSlide.tsx
+++ src/components/ClothingSlide.tsx
@@ -7,7 +7,7 @@ interface ClothingProps {
 const ClothingSlide = ({title, background, children}: ClothingProps) => {
     return(
         <div className="group relative h-screen w-1/3">
-            <div className={`relative ${background} group-hover:ease-in-out duration-300 top-[0px] bg-cover bg-blend-overlay bg-stone-800 h-full w-full max-w-full group-hover:top-[-120px]`}/>
+            <div className={`relative ${background} group-hover:ease-in-out duration-300 top-[0px] bg-cover bg-blend-overlay bg-stone-600 h-full w-full max-w-full group-hover:top-[-120px]`}/>
             <div className="absolute bottom-[-120px] group-hover:ease-in-out duration-300 h-[120px] group-hover:bottom-[0px]">
                 <h1 className="text-4xl">{title}</h1>
                 {children}
