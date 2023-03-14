0e302501afef24079db42ce55c294b8754cfa5cd
Hajdu Péter <hajdupetke@gmail.com>
2023-03-14 14:46:44 +0100

    removed state, fixed code format

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import ClothingSlide from './components/ClothingSlide'
+import ClothingSlide from './components/ClothingSlide';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="flex">
-    <ClothingSlide title='Male' background='bg-male'>
-      <ul>
-        <li>Coats</li>
-        <li>T-shirts</li>
-      </ul>
-    </ClothingSlide>
-    <ClothingSlide title='Female' background='bg-female'>
-      <ul>
-        <li>Coats</li>
-        <li>T-shirts</li>
-      </ul>
-    </ClothingSlide>
-    <ClothingSlide title='Kids' background='bg-kid'>
-      <ul>
-        <li>Coats</li>
-        <li>T-shirts</li>
-      </ul>
-    </ClothingSlide>
-    
+      <ClothingSlide title="Male" background="bg-male">
+        <ul>
+          <li>Coats</li>
+          <li>T-shirts</li>
+        </ul>
+      </ClothingSlide>
+      <ClothingSlide title="Female" background="bg-female">
+        <ul>
+          <li>Coats</li>
+          <li>T-shirts</li>
+        </ul>
+      </ClothingSlide>
+      <ClothingSlide title="Kids" background="bg-kid">
+        <ul>
+          <li>Coats</li>
+          <li>T-shirts</li>
+        </ul>
+      </ClothingSlide>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
