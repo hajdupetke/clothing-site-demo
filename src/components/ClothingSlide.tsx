5957bbc3cd2d935e27b3e16c33429500de46846d
Hajdu Péter <hajdupetke@gmail.com>
2023-03-14 13:30:43 +0100

    Added airbnb eslint config, added prettier

--- src/components/ClothingSlide.tsx
+++ src/components/ClothingSlide.tsx
@@ -1,22 +1,21 @@
 interface ClothingProps {
-    title: string,
-    background: string,
-    children: JSX.Element
+  title: string;
+  background: string;
+  children: JSX.Element;
 }
 
-const ClothingSlide = ({title, background, children}: ClothingProps) => {
-    return(
-        <div className="group relative h-screen w-1/3">
-            <div className={`relative ${background} group-hover:ease-in-out duration-300 top-[0px] bg-cover bg-blend-overlay bg-stone-600 h-full w-full max-w-full group-hover:top-[-120px]`}/>
-            <div className="absolute bottom-[-120px] group-hover:ease-in-out duration-300 h-[120px] group-hover:bottom-[0px]">
-                <h1 className="text-4xl">{title}</h1>
-                <div>
-                {children}
-                </div>
-            </div>
-            
-        </div>
-    )
+function ClothingSlide({ title, background, children }: ClothingProps) {
+  return (
+    <div className="group relative h-screen w-1/3">
+      <div
+        className={`relative ${background} group-hover:ease-in-out duration-300 top-[0px] bg-cover bg-blend-overlay bg-stone-600 h-full w-full max-w-full group-hover:top-[-120px]`}
+      />
+      <div className="absolute bottom-[-120px] group-hover:ease-in-out duration-300 h-[120px] group-hover:bottom-[0px]">
+        <h1 className="text-4xl">{title}</h1>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ClothingSlide
+export default ClothingSlide;
