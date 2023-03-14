99cddb2743f622238ec7a3b745a5caee4d231d60
Hajdu Péter <hajdupetke@gmail.com>
2023-03-14 13:12:38 +0100

    Installed eslint

--- src/components/ClothingSlide.tsx
+++ src/components/ClothingSlide.tsx
@@ -10,7 +10,9 @@ const ClothingSlide = ({title, background, children}: ClothingProps) => {
             <div className={`relative ${background} group-hover:ease-in-out duration-300 top-[0px] bg-cover bg-blend-overlay bg-stone-600 h-full w-full max-w-full group-hover:top-[-120px]`}/>
             <div className="absolute bottom-[-120px] group-hover:ease-in-out duration-300 h-[120px] group-hover:bottom-[0px]">
                 <h1 className="text-4xl">{title}</h1>
+                <div>
                 {children}
+                </div>
             </div>
             
         </div>
